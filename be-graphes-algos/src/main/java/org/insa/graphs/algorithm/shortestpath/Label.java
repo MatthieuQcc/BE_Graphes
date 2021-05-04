41f714f983f314e4847b713c4cf321456293fa04
MatthieuQcc <75421102+MatthieuQcc@users.noreply.github.com>
2021-05-04 17:43:00 +0200

    Update Label.java

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -1,9 +1,37 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-public class Label extends DijkstraAlgorithm {
+import org.insa.graphs.model.Node;
 
-    public Label(ShortestPathData data) {
-        super(data);
-     }
-
+public class Label implements Comparable<Label> {
+	public Node sommetCourant;
+	public boolean marque;
+	public float cout;
+	public Node pere;
+	
+	public Label(Node noeud){
+		this.sommetCourant = noeud;
+		this.marque = false;
+		this.cout = Float.POSITIVE_INFINITY;
+		this.pere = null; 
+	}
+	
+	public float getCost() {
+		return this.cout;
+	}
+	
+	public boolean getMarque() {
+		return this.marque;
+	}
+	
+	public Node getNode() {
+		return this.sommetCourant;
+	}
+	
+	public Node getPere() {
+		return this.pere;
+	}
+	
+	
+	
 }
+
