e8a1de148edc512d7c3cc6cb650fce42e95559cc
MatthieuQcc <75421102+MatthieuQcc@users.noreply.github.com>
2021-05-24 12:34:26 +0200

    Update AStarAlgorithm.java

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -8,7 +8,9 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
         super(data);
     }
 
+    
     // On redéfinit la méthode nouveau label
+    
     public Label nouveauLab(Node noeud) {
     	ShortestPathData data = this.getInputData() ;
     	return new LabelStar(noeud, data, this.data.getMaximumSpeed());
